a2b86c1f3feddb7f75a035331cc8ccc5e0300bde
Davide Vitale <davidee.vitalee@gmail.com>
2021-03-10 15:51:55 +0100

    aggiunta battaglia-navale

---
+++ battaglia-navale/field.js
@@ -0,0 +1,64 @@
+const myFunction = (field, ship) => {
+  const x = ship.x
+  const y = ship.y
+  if (ship.vertical) {
+    for (let i = 0; i < ship.maxHp; i++) {
+      if (!field[y + i] || !field[y + i][x] || field[y + i][x].ship) {
+        return false
+      }
+    }
+  } else {
+    for (let i = 0; i < ship.maxHp; i++) {
+      if (!field[y] || !field[y][x + i] || field[y][x + i].ship) {
+        return false
+      }
+    }
+  }
+  return true
+}
+
+module.exports.fillField = (w, h, s, field, ships) => {
+  let count = 0
+
+  for (let y = 0; y < h; y++) {
+    const row =  []
+    for (let x = 0; x < w; x++) {
+      row.push({
+        team: null,
+        x,
+        y,
+        ship: null,
+        shipId: null,
+        hit: false
+      })
+    }
+    field.push(row)
+  }
+
+  while (count < s) {
+    const x = Math.floor(Math.random() * w)
+    const y = Math.floor(Math.random() * h)
+    const maxHp = Math.floor(Math.random() * (6 - 2)) + 2
+    const vertical = Math.random() < 0.5
+    const ship = {
+      id: `nave ${count}`,
+      x,
+      y,
+      vertical,
+      maxHp,
+      curHp: maxHp,
+      alive: true,
+      killer: null
+    }
+
+    if (myFunction(field, ship)) {
+      ships.push(ship)
+      for (let e = 0; e < ship.maxHp; e ++) {
+        const x = ship.vertical ? ship.x : ship.x + e
+        const y = ship.vertical ? ship.y + e : ship.y
+        field[y][x].ship = ship
+      }
+      count ++
+    }
+  }
+}
---
+++ battaglia-navale/index.js
@@ -0,0 +1,148 @@
+const express = require("express")
+const app = new express
+const sqlite = require("sqlite3").verbose()
+const db = new sqlite.Database(":memory:")
+const { fillField } = require("./field")
+
+const port = 8080
+
+app.use(express.json())
+
+const field = []
+const ships = []
+
+const w = process.argv[2] || 10
+const h = process.argv[3] || 10
+const s = process.argv[4] || 10
+
+fillField(w, h, s, field, ships)
+
+db.run(`CREATE TABLE IF NOT EXISTS teams (
+  name VARCHAR(255) NOT NULL,
+  score INT NOT NULL,
+  lastlog VARCHAr(255) NOT NULL
+)`)
+
+app.get("/", ({ query: { format } }, res) => {
+  if (format === "json") {
+    res.json({ field })
+  } else {
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>battaglia navale</title>
+      <style>
+        table, td, th {
+          border: 1px solid black;
+        }
+        
+        table {
+          width: 50%;
+          border-collapse: collapse;
+        }
+      </style>
+    </head>
+    <body>
+      <table>
+        <tbody>
+          ${field.map(row => `<tr>${row.map(cell => `<td>${cell.ship ? cell.ship.id : "acqua"}</td>`).join("")}</tr>`).join("")}
+        </tbody>
+      </table>
+    </body>
+    </html>
+		`)
+  }
+})
+
+app.get("/score", ({ body: { team } }, res) => {
+  db.get("SELECT * FROM teams WHERE name = ?", team, (err, row) => {
+    if (err) {
+      throw err
+    } if (row) {
+      res.json({
+        team: row.name,
+        score: row.score
+      })
+    } else {
+      res.sendStatus(404)
+    }
+  })
+})
+
+app.get("/fire", ({ body: { x, y, team } }, res) => {
+  let hit = false
+  const cell = field[y - 1][x - 1]
+  const time = new Date().getTime()
+  db.get("SELECT * FROM teams WHERE name = ?", team, (err, row) => {
+    if (err) {
+      throw err
+    } else if (!row) {
+      db.run("INSERT INTO teams (name, score, lastlog) VALUES (?, ?, ?)", [team, 0, time], (err) => {
+        if (err) {
+          throw err
+        }
+        if (cell.ship) {
+          let score = 1
+          hit = true
+          cell.ship.curHp -= 1
+          if (cell.ship.curHp === 0) {
+            cell.ship.alive = false
+            score += 2
+            cell.ship.killer = row.name
+          }
+          cell.ship = null
+          db.run("UPDATE teams SET score = ? WHERE name = ?", [score, team], (err) => {
+            if (err) {
+              throw err
+            }
+          })
+        }
+        res.json({
+          x, y, team, hit
+        })
+      })
+    } else if ((time - row.lastlog) < 1000) {
+      const score = row.score
+      db.run("UPDATE teams SET score = ? WHERE name = ?", [score - 1, team], (err) => {
+        if (err) {
+          throw err
+        }
+      })
+      res.json({ message: "too fast" })
+    } else if ((time - row.lastlog) > 1000) {
+      db.run("UPDATE teams SET lastlog = ? WHERE name = ?", [time, team], (err) => {
+        if (err) {
+          throw err
+        }
+      })
+      if (cell.ship) {
+        let score = row.score
+        hit = true
+        cell.ship.curHp -= 1
+        if (cell.ship.curHp === 0) {
+          cell.ship.alive = false
+          score += 3
+          cell.ship.killer = row.name
+        } else {
+          score += 1
+        }
+        cell.ship = null
+        db.run("UPDATE teams SET score = ? WHERE name = ?", [score, team], (err) => {
+          if (err) {
+            throw err
+          }
+        })
+      }
+      res.json({
+        x, y, team, hit
+      })
+    }
+  })
+})
+
+app.all("*", (req, res) => {
+  res.sendStatus(404)
+})
+
+app.listen(port, () => console.log("Listening on port", port))
